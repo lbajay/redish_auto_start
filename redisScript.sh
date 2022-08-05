0bcef2c766d66206a80b62814255f29dbf04e95f
lbajay <102253092+lbajay@users.noreply.github.com>
2022-08-05 15:32:18 +0530

    Add files via upload

---
+++ redisScript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+a=$(redis-cli -p 6379 PING)
+if [ "$a" = "PONG" ]
+then
+   echo "$a"
+else
+   echo "unfortunately stopped , redis is starting again"
+    sudo systemctl start redis.service
+fi
+
