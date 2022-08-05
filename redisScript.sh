9144b51d1e244982d828e5287545144bb7fc02cc
lbajay <102253092+lbajay@users.noreply.github.com>
2022-08-05 15:45:27 +0530

    Update redisScript.sh

--- redisScript.sh
+++ redisScript.sh
@@ -6,5 +6,7 @@ then
 else
    echo "unfortunately stopped , redis is starting again"
     sudo systemctl start redis.service
-fi
+  cd /var/www/html/datamanager/
+php artisan run:command --command="websocket:serve"
 
+fi
